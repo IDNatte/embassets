e59f51619c4b9da6e5814f36f75e97fef1dc2692
IDNatte <idnatte@gmail.com>
2025-09-22 12:16:16 +0800

    init commit of embassets

---
+++ embassets.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.0.1"
+author        = "IDNatte"
+description   = "simple assets embedding in binary"
+license       = "BSD-3-Clause"
+srcDir        = "src"
+
+
+# Dependencies
+
+requires "nim >= 2.2.4"
---
+++ src/embassets.nim
@@ -0,0 +1,60 @@
+import std/[exitprocs, strformat, macros, tables, os]
+
+proc loadAssets*(data: seq[Table[system.string, system.string]]): string =
+  try:
+    let tempDir = joinPath(getTempDir(), fmt"mapps-{getCurrentProcessId()}")
+    if not dirExists(tempDir):
+      createDir(tempDir)
+
+    for assets in data:
+      let relPath = assets["relative_path"]
+      let fullPath = joinPath(tempDir, relPath)
+
+      createDir(parentDir(fullPath))
+      writeFile(joinPath(tempDir, assets["relative_path"]), assets["data"])
+
+    addExitProc(proc() =
+      try:
+        removeDir(tempDir)
+      except OSError:
+        discard
+    )
+
+    return tempDir
+
+  except OSError:
+    discard
+
+macro embedAssets*(src: static[string]): untyped =
+  when system.hostOS == "windows":
+    let cwd = staticExec("powershell -Command \"(Get-Location).path\"")
+  elif system.hostOS == "linux":
+    let cwd = staticExec("pwd")
+  else:
+    let cwd = newLit(nnkNilLit)
+
+  let absPath =  joinPath(cwd, src)
+
+  var elements = newSeq[NimNode]()
+  for path in walkDirRec(absPath):
+    let filename = fmt"{splitFile(path).name}{splitFile(path).ext}"
+    let relPath = path[(absPath.len + 1) .. path.len - 1]
+
+    let meta = newCall(bindSym"toTable",
+      nnkTableConstr.newTree(
+        nnkExprColonExpr.newTree(
+          newLit("filename"), newLit(filename)
+        ),
+        nnkExprColonExpr.newTree(
+          newLit("relative_path"), newLit(relPath)
+        ),
+        nnkExprColonExpr.newTree(
+          newLit("data"),
+          newCall(bindSym"staticRead", newLit(path))
+        )
+      )
+    )
+
+    elements.add(meta)
+
+  return newCall(bindSym"@", nnkBracket.newTree(elements))
